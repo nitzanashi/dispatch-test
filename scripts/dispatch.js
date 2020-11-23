c5974f3a73c16543f16be75caaac055cbe7d7fb9
Nitzan Nashi <nitzan.nashi@zattoo.com>
2020-11-23 09:46:57 +0100

    tweak

--- scripts/dispatch.js
+++ scripts/dispatch.js
@@ -1,42 +1,23 @@
 import fetch from 'node-fetch';
-import {exec} from 'child_process';
-import {promisify} from 'util';
 
 const {
     GITHUB_TOKEN,
-    GITHUB_REPOSITORY,
-    GITHUB_API_URL,
 } = process.env;
 
-const execPromise = promisify(exec);
-
+const GITHUB_API_URL = 'https://api.github.com';
 const BRANCH = 'release/seo';
 const WORKFLOW = 'lighthouse.yml';
-const BASE_URL = `${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/actions/workflows/${WORKFLOW}/dispatches`;
+const BASE_URL = `${GITHUB_API_URL}/repos/nitzanashi/dispatch-test`;
+const DISPATCH_URL = `${BASE_URL}/actions/workflows/${WORKFLOW}/dispatches`;
 
 (async () => {
-    let ref;
-
-    try {
-        const test = (await execPromise('git branch -r')).stdout;
-        console.log(test);
-        ref = (await execPromise(`git show-ref remotes/origin/${BRANCH}`)).stdout.split(' ')[0];
-    } catch (e) {
-        console.log(e);
-        console.log('Branch does not exists nothing to do.');
-        process.exit(0);
-    }
-
-    console.log(`The ref for ${BRANCH} is: ${ref}`);
-
     /**
      * dispatch lighthouse.
      * @see https://docs.github.com/en/free-pro-team@latest/rest/reference/actions#create-a-workflow-dispatch-event
      */
-    const request = await fetch(BASE_URL, {
+    const request = await fetch(DISPATCH_URL, {
         body: JSON.stringify({
-            ref,
-            workflow_id: WORKFLOW, // DO WE NEED THIS?
+            ref: BRANCH,
         }),
         headers: {
             'Content-Type': 'application/json',
@@ -45,7 +26,7 @@ const BASE_URL = `${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/actions/workflows
         method: 'POST',
     });
 
-    if (request.status === 200) {
+    if (request.status === 204) {
         console.log('success');
         process.exit(0);
     }
